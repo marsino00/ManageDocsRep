ed0bfe559784aaa8771f310ac94fa946dbdab8f2
marsino00 <95243586+marsino00@users.noreply.github.com>
2025-02-18 17:20:53 +0100

    Changes in configuration files to align with relatives routes

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,18 @@
+const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  resolver: {
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => {
+          return path.join(__dirname, 'src', String(name));
+        },
+      },
+    ),
+  },
+  watchFolders: [path.resolve(__dirname, 'src')],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
